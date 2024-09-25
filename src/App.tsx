2e21ba141260974aefb4fdc189ee464841db1d4f
accguy <accguy2190@naver.com>
2024-09-25 15:10:40 +0900

    chore: ESLint-airbnb 세팅 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let a = 3;
+  if (a == 3) {
+    // == 사용 못하게하는 eslint 테스트용 코드
+    console.log("a는 3입니다");
+  }
+  let b = 3;
+  if (b == 3) {
+    console.log("b는 3입니다");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
