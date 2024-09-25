94f3688144ddefa1cf9994b42d2d07dc27eba59d
page1597 <gimm08377@gmail.com>
2024-09-25 15:25:38 +0900

    chore: Firebase 세팅 - close #1
    - Firebase 관련 테스트 코드 동작 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,42 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect } from "react";
 import "./App.css";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "./firebase";
 
 function App() {
-  let a = 3;
+  const a = 3;
   if (a == 3) {
     // == 사용 못하게하는 eslint 테스트용 코드
     console.log("a는 3입니다");
   }
-  let b = 3;
+  const b = 3;
   if (b == 3) {
     console.log("b는 3입니다");
   }
 
+  // firebase 사용 테스트
+  const getDocs = async () => {
+    try {
+      const docRef = doc(db, "concert", "concert123");
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+      } else {
+        console.log("No such document!");
+      }
+    } catch (e) {
+      console.error("에러", e);
+    }
+  };
+
+  useEffect(() => {
+    getDocs();
+  }, []);
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>muna</h1>
     </div>
   );
 }
---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
+};
+
+export const app = initializeApp(firebaseConfig);
+export const firebaseAuth = getAuth(); // app 인스턴스를 getAuth에 전달
+export const db = getFirestore(app); // app 인스턴스를 getFirestore에 전달
