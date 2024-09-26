98e224d4163ac3b29ee9c231b3d65543e9a55b4e
page1597 <gimm08377@gmail.com>
2024-09-26 13:09:23 +0900

    fix: Redux Toolkit 세팅 린트 에러 헤결

--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -19,6 +19,19 @@ const initialState: CounterState = {
   status: "idle",
 };
 
+// Thunk는 데이터 가져오기와 같은 비동기 로직에 흔히 사용됩니다.
+// `createAsyncThunk` 메서드는 promise 기반의 비동기 작업을 처리하는 thunk를 생성합니다.
+// 이 예시에서는 mock 비동기 요청을 만들어 결과를 반환합니다.
+// `createSlice.extraReducers` 필드에서 이러한 액션을 처리하여 결과로 상태를 업데이트할 수 있습니다.
+export const incrementAsync = createAsyncThunk(
+  "counter/fetchCount",
+  async (amount: number) => {
+    const response = await fetchCount(amount);
+    // 반환된 값은 `fulfilled` 액션 payload가 됩니다.
+    return response.data;
+  }
+);
+
 // 슬라이스는 Redux 리듀서 로직을 포함하며, 상태를 업데이트하기 위한 액션을 생성합니다.
 // 이러한 액션을 디스패치하여 상태 업데이트를 트리거할 수 있습니다.
 export const counterSlice = createSlice({
@@ -44,7 +57,7 @@ export const counterSlice = createSlice({
   // createAsyncThunk나 다른 슬라이스에서 생성된 액션을 포함합니다.
   extraReducers: (builder) => {
     builder
-      // 아래 정의된 `incrementAsync` thunk에서 생성된 액션 타입을 처리합니다.
+      // 위에 정의된 `incrementAsync` thunk에서 생성된 액션 타입을 처리합니다.
       // 이를 통해 슬라이스 리듀서가 요청 상태 및 결과로 상태를 업데이트할 수 있게 됩니다.
       .addCase(incrementAsync.pending, (state) => {
         state.status = "loading";
@@ -74,24 +87,11 @@ export const selectStatus = (state: RootState) => state.counter.status;
 // 아래 함수는 thunk라고 하며, 동기 및 비동기 로직을 모두 포함할 수 있습니다.
 // `dispatch`와 `getState`에 접근할 수 있으며, 일반 액션처럼 디스패치할 수 있습니다: `dispatch(incrementIfOdd(10))`.
 // 현재 상태에 따라 조건부로 액션을 디스패치하는 예시입니다.
-export const incrementIfOdd = (amount: number): AppThunk => {
-  return (dispatch, getState) => {
+export const incrementIfOdd =
+  (amount: number): AppThunk =>
+  (dispatch, getState) => {
     const currentValue = selectCount(getState());
     if (currentValue % 2 === 1) {
       dispatch(incrementByAmount(amount));
     }
   };
-};
-
-// Thunk는 데이터 가져오기와 같은 비동기 로직에 흔히 사용됩니다.
-// `createAsyncThunk` 메서드는 promise 기반의 비동기 작업을 처리하는 thunk를 생성합니다.
-// 이 예시에서는 mock 비동기 요청을 만들어 결과를 반환합니다.
-// `createSlice.extraReducers` 필드에서 이러한 액션을 처리하여 결과로 상태를 업데이트할 수 있습니다.
-export const incrementAsync = createAsyncThunk(
-  "counter/fetchCount",
-  async (amount: number) => {
-    const response = await fetchCount(amount);
-    // 반환된 값은 `fulfilled` 액션 payload가 됩니다.
-    return response.data;
-  }
-);
