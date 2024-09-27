fa303c52c34cd43cf117fb90fc5a1beb2cbd800b
page1597 <gimm08377@gmail.com>
2024-09-27 13:57:15 +0900

    chore: 전역 폰트 설정 - #5

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import "./App.css";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "./firebase";
 import LoginForm from "./components/LoginForm";
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-import "normalize.css";
 import "./styles/global.scss";
-
+import "normalize.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
