dfeda0028fcdb23be3b4828fee5b6e28733d4e88
page1597 <gimm08377@gmail.com>
2024-09-30 16:23:49 +0900

    feat: Dropdown 외부 영역 클릭시 닫힘 #8

--- src/components/common/Dropdown/DropdownMenu.tsx
+++ src/components/common/Dropdown/DropdownMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./Dropdown.module.scss";
 import Button from "../Button/Button";
 import KebabMenuIcon from "../../../assets/svg/KebabMenuIcon";
@@ -15,6 +15,7 @@ export default function DropdownMenu({
   label,
 }: DropdownMenuProps) {
   const [isOpen, setIsOpen] = useState(false);
+  const dropdownMenuRef = useRef<HTMLDivElement | null>(null);
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
@@ -25,8 +26,25 @@ export default function DropdownMenu({
     onSelect(value);
   };
 
+  const handleClickOutside = (e: MouseEvent) => {
+    if (
+      dropdownMenuRef.current &&
+      !dropdownMenuRef.current.contains(e.target as Node)
+      // 클릭한 위치가 드롭다운이 아닐 때
+    ) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className={styles.dropdown}>
+    <div className={styles.dropdown} ref={dropdownMenuRef}>
       {label}
       <Button
         label='menu'
--- src/components/common/Dropdown/DropdownSelect.tsx
+++ src/components/common/Dropdown/DropdownSelect.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./Dropdown.module.scss";
 import Button from "../Button/Button";
 import CaretIcon from "../../../assets/svg/CaretIcon";
@@ -8,7 +8,7 @@ interface DropdownSelectProps {
   onSelect: (value: string) => void;
   label?: string;
 }
-
+// TODO: 로직 훅으로 관리하기 / 추후 고려
 export default function DropdownSelect({
   options = [],
   onSelect,
@@ -16,6 +16,7 @@ export default function DropdownSelect({
 }: DropdownSelectProps) {
   const [isOpen, setIsOpen] = useState(false);
   const [selected, setSelected] = useState<string>(options[0]);
+  const dropdownSelectRef = useRef<HTMLDivElement | null>(null);
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
@@ -27,8 +28,24 @@ export default function DropdownSelect({
     onSelect(value);
   };
 
+  const handleClickOutside = (e: MouseEvent) => {
+    if (
+      dropdownSelectRef.current &&
+      !dropdownSelectRef.current.contains(e.target as Node)
+    ) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className={styles.dropdown}>
+    <div className={styles.dropdown} ref={dropdownSelectRef}>
       {label}
       <Button
         label={selected}
@@ -38,7 +55,7 @@ export default function DropdownSelect({
         size='md'
       />
       {isOpen && (
-        <ul className={styles.dropdown_ul}>
+        <ul className={`${styles.dropdown_ul}`}>
           {options.map((option) => (
             <li
               key={option}
