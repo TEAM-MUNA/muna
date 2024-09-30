4f719d4532d12cfbe449309e78ab01ba770432d7
accguy <accguy2190@naver.com>
2024-10-01 01:34:27 +0900

    ui: StarTag 컴포넌트 추가 #6

---
+++ src/components/common/StarTag/StarTag.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./StarTag.module.scss";
+import StarIcon from "../../../assets/svg/StarIcon";
+
+interface StarTagProps {
+  rating?: number;
+}
+
+export default function StarTag({ rating = 0 }: StarTagProps) {
+  const fixedRating = rating.toFixed(1);
+
+  return (
+    <div className={styles.container}>
+      {rating ? (
+        <>
+          <div>{fixedRating}</div>
+          <StarIcon size='14px' active />
+        </>
+      ) : (
+        <StarIcon size='12.84px' />
+      )}
+    </div>
+  );
+}
