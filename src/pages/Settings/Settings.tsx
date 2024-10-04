a96047cf954c23283c99c9e0261f6a910a64b41f
dia <jdyoung1031@gmail.com>
2024-10-04 18:59:48 +0900

    ui: 드롭다운 스타일 추가 - 라인없는 형태 - outline props false

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import ImageInput from "../../components/common/ImageUploader/ImageUploader";
 import LikeButton from "../../components/common/Button/LikeButton";
+import DropdownSelect from "../../components/common/Dropdown/DropdownSelect";
 
 export default function Settings() {
+  const handleSelect = () => {
+    console.log("select test");
+  };
   return (
     <div>
       <h1>Settings</h1>
       <ImageInput />
       <LikeButton />
+      <DropdownSelect
+        options={["option1", "option2"]}
+        onSelect={handleSelect}
+        outline={false}
+      />
     </div>
   );
 }
