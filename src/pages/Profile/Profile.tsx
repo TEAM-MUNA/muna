a04b196a20b906839ca43261201d751ec0b2633a
dia <jdyoung1031@gmail.com>
2024-10-04 11:45:21 +0900

    ui: 북마크리스트 기본요소

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,54 +1,59 @@
 import React from "react";
-import DropdownMenu from "../../components/common/Dropdown/DropdownMenu";
+import styles from "./Profile.module.scss";
+import Avatar from "../../components/common/Avatar/Avatar";
+import Button from "../../components/common/Button/Button";
+import SettingsIcon from "../../assets/svg/SettingsIcon";
+import Tab from "../../components/common/Tab/Tab";
 import DropdownSelect from "../../components/common/Dropdown/DropdownSelect";
-import ReviewCard from "../../components/common/ReviewCard/ReviewCard";
 import PosterCard from "../../components/common/PosterCard/PosterCard";
-import ReviewGalleryCard from "../../components/common/ReviewGalleryCard/ReviewGalleryCard";
-import StarIcon from "../../assets/svg/StarIcon";
+
+// import ReviewCard from "../../components/common/ReviewCard/ReviewCard";
+// import ReviewGalleryCard from "../../components/common/ReviewGalleryCard/ReviewGalleryCard";
 
 export default function Profile() {
-  const dropdownMenuOptions = ["수정", "삭제"];
-  const handleDropdownMenu = () => {};
-
-  const dropdownSelectOptions = ["공연전체", "진행중", "진행완료"];
+  const tabList: (string | [string, number | null])[] = [
+    ["북마크한 공연", null],
+    ["나의 후기", 10],
+  ];
+  const stateSelectOptions = ["공연전체", "진행중", "진행완료"];
+  const orderSelectOptions = ["최신순", "진행중", "진행완료"];
   const handleDropdownSelect = () => {};
 
   return (
     <div>
-      <h1>Profile</h1>
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <StarIcon size='40' />
-        <StarIcon active size='40' />
-        <StarIcon size='24' />
-        <StarIcon active size='24' />
-        <StarIcon size='20' />
-        <StarIcon active size='20' />
-        <StarIcon size='14' />
-        <StarIcon active size='14' />
-      </div>
-      <DropdownMenu
-        onSelect={handleDropdownMenu}
-        options={dropdownMenuOptions}
-      />
-      <DropdownSelect
-        onSelect={handleDropdownSelect}
-        options={dropdownSelectOptions}
-      />
-      <ReviewCard reviewLink='#' />
-      <ReviewCard reviewLink='#' page='concert' />
-      <ReviewCard reviewLink='#' page='profile' />
-      <div>
-        <PosterCard concertLink='#' isBookmarked />
-        <PosterCard concertLink='#' />
-        <PosterCard concertLink='#' />
-        <PosterCard concertLink='#' />
-      </div>
-      <div>
+      <h1 className='sr_only'>00님의 프로필페이지</h1>
+      <article className={styles.top}>
+        <Avatar size='lg' />
+        <Button label='프로필 설정' iconOnly={<SettingsIcon />} />
+      </article>
+      {/* TODO: 탭 선택시 > 현재 활성화 탭 필요 */}
+      <Tab tabList={tabList} withNumber />
+      <section className={`${styles.tab_content} ${styles.bookmark}`}>
+        <DropdownSelect
+          onSelect={handleDropdownSelect}
+          options={stateSelectOptions}
+        />
+        <DropdownSelect
+          onSelect={handleDropdownSelect}
+          options={orderSelectOptions}
+        />
+        <ul>
+          <li>
+            <PosterCard concertLink='#' isBookmarked />
+          </li>
+        </ul>
+      </section>
+
+      {/* <div>
         <ReviewGalleryCard reviewLink='#' hasMultiImages />
         <ReviewGalleryCard reviewLink='#' />
         <ReviewGalleryCard reviewLink='#' />
         <ReviewGalleryCard reviewLink='#' />
-      </div>
+      </div> */}
+      {/* <ReviewCard reviewLink='#' />
+      <ReviewCard reviewLink='#' page='concert' />
+      <ReviewCard reviewLink='#' page='profile' />
+       */}
     </div>
   );
 }
