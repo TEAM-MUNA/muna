01b2d580431fafd5dbfc4ffbc0a500633fa4d41b
page1597 <gimm08377@gmail.com>
2024-10-04 16:48:49 +0900

    feat: 로그인 기능 추가

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,13 +1,49 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
+import { FirebaseError } from "firebase/app";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../app/store";
+import { loginAsync } from "../../slices/authSlice";
 import logo from "../../assets/img/logo.png";
 import styles from "./Login.module.scss";
 import Input from "../../components/common/Input/Input";
 import Button from "../../components/common/Button/Button";
+import useInput from "../../hooks/useInput";
 
 export default function Login() {
+  const { value: email, onChange: onEmailChange } = useInput("");
+  const { value: password, onChange: onPasswordChange } = useInput("");
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const loadingToastId = toast.loading("로그인 중...");
+
+    try {
+      await dispatch(loginAsync({ email, password })).unwrap();
+      toast.success("로그인에 성공하였습니다.", { id: loadingToastId });
+      setTimeout(() => {
+        navigate("/");
+      }, 500);
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        toast.error(error.message, { id: loadingToastId });
+      } else {
+        toast.error("이메일 또는 비밀번호를 다시 확인해주세요.", {
+          id: loadingToastId,
+        });
+      }
+    }
+  };
+
   return (
     <article className={styles.login}>
+      <div>
+        <Toaster />
+      </div>
       <header className={styles.header}>
         <h1>
           <img width={107} src={logo} alt='muna 로고' />
@@ -16,11 +52,23 @@ export default function Login() {
       </header>
       <main>
         <section>
-          <form className={styles.form}>
-            <Input label='이메일' placeholder='이메일 주소를 입력해 주세요.' />
-            <Input label='비밀번호' placeholder='비밀번호를 입력해 주세요.' />
+          <form className={styles.form} onSubmit={handleLogin}>
+            <Input
+              value={email}
+              onChange={onEmailChange}
+              label='이메일'
+              placeholder='이메일 주소를 입력해 주세요.'
+            />
+            <Input
+              type='password'
+              value={password}
+              onChange={onPasswordChange}
+              label='비밀번호'
+              placeholder='비밀번호를 입력해 주세요.'
+            />
             <Button
               size='xl'
+              type='submit'
               color='primary'
               fullWidth
               label='로그인'
@@ -32,6 +80,7 @@ export default function Login() {
           </form>
         </section>
       </main>
+      {/* <LoginForm /> */}
     </article>
   );
 }
