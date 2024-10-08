197de0faa8238fc56478f8da751f15cf16c3e0d1
page1597 <gimm08377@gmail.com>
2024-10-08 09:19:57 +0900

    ui: 회원가입 페이지 화면 구현

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,5 +1,42 @@
 import React from "react";
+import { Toaster } from "react-hot-toast";
+import styles from "./Signup.module.scss";
+import ImageUploader from "../../components/common/ImageUploader/ImageUploader";
+import Input from "../../components/common/Input/Input";
+import Button from "../../components/common/Button/Button";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  return (
+    <article className={styles.signup}>
+      <Toaster />
+      <section>
+        <h2 className={styles.title}>회원가입</h2>
+        <form className={styles.form} onSubmit={() => {}}>
+          <span className={styles.image_uploader}>
+            <ImageUploader />
+          </span>
+          <Input label='이메일' placeholder='이메일 주소를 입력해 주세요.' />
+          <Input
+            type='password'
+            label='비밀번호'
+            placeholder='비밀번호를 입력해 주세요.'
+          />
+          <Input
+            type='password'
+            label='비밀번호 확인'
+            placeholder='현재 비밀번호를 입력해 주세요.'
+          />
+          <Input label='사용자 닉네임' placeholder='닉네임을 입력해 주세요.' />
+          <Button
+            size='xl'
+            type='submit'
+            color='black'
+            fullWidth
+            label='회원가입'
+            className={styles.button}
+          />
+        </form>
+      </section>
+    </article>
+  );
 }
