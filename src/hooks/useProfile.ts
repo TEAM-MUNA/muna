7f91b8402963a8e23bdbe2398de83f072fabf25b
dia <jdyoung1031@gmail.com>
2024-10-11 00:39:48 +0900

    feat: useProfile 추가 (기능변경) - currentUser의 정보를 가져오는게 아닌, id값에 따른 해당 회원 정보를 가져오도록

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -1,14 +1,33 @@
-import { useSelector } from "react-redux";
-import { RootState } from "../app/store";
+// hooks/useProfile.ts
+import { useEffect, useState } from "react";
+import { getUserFromFirebase } from "../api/authAPI";
+import { UserProps } from "../types/userProps";
 
-const useProfile = () => {
-  const user = useSelector((state: RootState) => state.auth.user);
-  return {
-    userId: user?.uid,
-    email: user?.email,
-    nickname: user?.nickname,
-    profileImage: user?.profileImage,
-  };
+const useProfile = (userId: string | undefined) => {
+  const [profile, setProfile] = useState<UserProps | null>(null);
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      if (userId) {
+        try {
+          const userProfile: UserProps | undefined =
+            await getUserFromFirebase(userId);
+          if (userProfile) {
+            setProfile(userProfile);
+          } else {
+            setProfile(null); // 사용자 정보가 없을 경우 null
+          }
+        } catch (error) {
+          console.error("프로필 정보를 가져오는 중 오류 발생:", error);
+          setProfile(null); // 오류 발생 시 null
+        }
+      }
+    };
+
+    fetchProfile(); // 프로필 정보 가져오기
+  }, [userId]);
+
+  return profile;
 };
 
 export default useProfile;
