2f336e182683fe96361036c29e2dea61899e8ce1
dia <jdyoung1031@gmail.com>
2024-10-11 01:05:06 +0900

    ui: 로딩중, 존재하지 않는 회원 UI 분리

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -5,6 +5,7 @@ import { UserProps } from "../types/userProps";
 
 const useProfile = (userId: string | undefined) => {
   const [profile, setProfile] = useState<UserProps | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchProfile = async () => {
@@ -17,6 +18,7 @@ const useProfile = (userId: string | undefined) => {
           } else {
             setProfile(null); // 사용자 정보가 없을 경우 null
           }
+          setLoading(false);
         } catch (error) {
           console.error("프로필 정보를 가져오는 중 오류 발생:", error);
           setProfile(null); // 오류 발생 시 null
@@ -27,7 +29,7 @@ const useProfile = (userId: string | undefined) => {
     fetchProfile(); // 프로필 정보 가져오기
   }, [userId]);
 
-  return profile;
+  return { profile, loading };
 };
 
 export default useProfile;
