21849bd6b0b8bd1dbb52cfedd9bffa3520567514
move-to-next <rlawodms95@naver.com>
2024-10-11 14:26:54 +0900

    ui: ReviewAvatar Link태그 추가 및 props에 userLink추가

--- src/components/common/Avatar/ReviewAvatar.tsx
+++ src/components/common/Avatar/ReviewAvatar.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./Avatar.module.scss";
 import { UserType, defaultUserType } from "../../../types/userType";
 
@@ -6,29 +8,28 @@ interface ReviewAvatarProps extends UserType {
 }
 
 export default function ReviewAvatar({
+  userLink = defaultUserType.userLink,
   nickname = defaultUserType.nickname,
   userId = defaultUserType.userId,
   profileImage = defaultUserType.profileImage,
   watchDate = "2020.10.21 관람",
 }: ReviewAvatarProps) {
   return (
-    <div className={styles.reviewAvatar}>
-      <div className={styles.reviewAvatarImg}>
-        <img src={profileImage} alt={nickname} className={styles.image} />
-      </div>
-      <div className={styles.reviewAvatarText}>
-        <div className={styles.reviewAvatarTextTop}>
-          <p className={`${styles.md} ${styles.black} ${styles.bold}`}>
-            {nickname}
-          </p>
-          <p className={`${styles.xs} ${styles.black_lighter}`}>{userId}</p>
+    <Link to={userLink} className={styles.reviewAvatar}>
+        <div className={styles.reviewAvatarImg}>
+          <img src={profileImage} alt={nickname} className={styles.image} />
         </div>
-        <p
-          className={`${styles.reviewAvatarWatchDay} ${styles.sm} ${styles.black_light} ${styles.regular}`}
-        >
-          {watchDate}
-        </p>
-      </div>
-    </div>
+        <div className={styles.reviewAvatarText}>
+          <div className={styles.reviewAvatarTextTop}>
+            <p className={`${styles.md} ${styles.black} ${styles.bold}`}>
+              {nickname}
+            </p>
+            <p className={`${styles.xs} ${styles.black_lighter}`}>{userId}</p>
+          </div>
+          <p className={`${styles.reviewAvatarWatchDay} ${styles.sm} ${styles.black_light} ${styles.regular}`}>
+            {watchDate}
+          </p>
+        </div>
+    </Link>
   );
 }
