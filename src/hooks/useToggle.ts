27dc78d2aebd80c010df43a64555f602ca84c8b2
page1597 <gimm08377@gmail.com>
2024-10-14 14:22:46 +0900

    fix: useToggle 훅에서 initialState 변경 시 상태가 업데이트되지 않는 문제 해결

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,7 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const useToggle = (initialState: boolean = false) => {
   const [isActive, setIsActive] = useState<boolean>(initialState);
+
+  useEffect(() => {
+    setIsActive(initialState);
+  }, [initialState]);
 
   const onToggle = () => {
     setIsActive(!isActive);
