b43c6c5b5f3ec8136de0c97875057e4ef0f084e6
dia <jdyoung1031@gmail.com>
2024-10-14 16:16:50 +0900

    fix: useInput 상태 업데이트 누락코드 - initialValue useEffect 추가하면서 누락함

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -14,6 +14,7 @@ const useInput = (
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = e.target.value;
+    setValue(newValue);
 
     if (!newValue) {
       setError(null);
