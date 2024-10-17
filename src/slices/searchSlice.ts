436c7a7af51b6b422a6261e3927f47a490df2604
accguy <accguy2190@naver.com>
2024-10-17 10:36:25 +0900

    feat: 최근 검색어 기능 추가
    - 검색어 로컬 스토리지 저장 및 삭제
    - ui 작업, 리팩토링 예정

--- src/slices/searchSlice.ts
+++ src/slices/searchSlice.ts
@@ -14,6 +14,29 @@ const searchSlice = createSlice({
   reducers: {
     setQuery(state, action) {
       state.query = action.payload;
+
+      // 공백 문자열 제거
+      const trimmedQuery = action.payload.trim();
+      if (trimmedQuery === "") return;
+
+      const storedQueries = JSON.parse(
+        localStorage.getItem("recentQueries") || "[]"
+      );
+
+      // 현재 날짜의 타임스탬프 가져오기
+      const timestamp = Date.now();
+
+      // 검색어 & 검색 날짜 객체로 저장
+      const newEntry = { query: trimmedQuery, date: timestamp };
+
+      const updatedQueries = [
+        newEntry,
+        ...storedQueries.filter(
+          (entry: typeof newEntry) => entry.query !== trimmedQuery
+        ),
+      ].slice(0, 5); // 5개만 저장
+
+      localStorage.setItem("recentQueries", JSON.stringify(updatedQueries));
     },
     clearQuery(state) {
       state.query = "";
