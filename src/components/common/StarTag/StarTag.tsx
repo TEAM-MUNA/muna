35c17845c315144664a0cd14dbd0543ff19087d9
move-to-next <rlawodms95@naver.com>
2024-10-21 15:06:53 +0900

    ui: 별점표시 위치수정

--- src/components/common/StarTag/StarTag.tsx
+++ src/components/common/StarTag/StarTag.tsx
@@ -3,21 +3,22 @@ import styles from "./StarTag.module.scss";
 import StarIcon from "../../../assets/svg/StarIcon";
 
 interface StarTagProps {
-  rating: number | undefined;
+  rating: number;
 }
 
-function StarTag({ rating }: StarTagProps) {
-  let fixedRating = "0.0";
-  if (typeof rating === "number") {
-    fixedRating = rating.toFixed(1);
-  }
+export default function StarTag({ rating = 0 }: StarTagProps) {
+  const fixedRating = rating.toFixed(1);
 
   return (
-    <div className={styles.star_tag}>
-      <StarIcon />
-      <span>{fixedRating}</span>
+    <div className={styles.container}>
+      {rating ? (
+        <>
+          <StarIcon size='14px' active />
+          <div>{fixedRating}</div>
+        </>
+      ) : (
+        <StarIcon size='12.84px' />
+      )}
     </div>
   );
 }
-
-export default StarTag;
