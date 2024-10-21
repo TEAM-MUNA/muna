8e626c12eda0934ef40df491689be9502b254eaf
page1597 <gimm08377@gmail.com>
2024-10-21 20:11:06 +0900

    chore: 사용되지 않는 파일 제거

--- src/slices/reviewSlice.ts
+++
@@ -1,6 +0,0 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { getUserFromFirebase } from "../api/firebase/authAPI";
-import {
-    getReviewListFromFirebase
-} from "../api/firebase/reviewAPI";
-import { ReviewType } from "../types/reviewType";
