9816c3ffc9fa41d2a02dfc6f7e4f53f75b44612d
move-to-next <rlawodms95@naver.com>
2024-10-21 15:42:36 +0900

    ui: StarTag 수정

--- src/components/common/StarTag/StarTag.tsx
+++ src/components/common/StarTag/StarTag.tsx
@@ -10,21 +10,9 @@ export default function StarTag({ rating = 0 }: StarTagProps) {
   const fixedRating = rating.toFixed(1);
 
   return (
-<<<<<<< HEAD
-    <div className={styles.container}>
-      {rating ? (
-        <>
-          <StarIcon size='14px' active />
-          <div>{fixedRating}</div>
-        </>
-      ) : (
-        <StarIcon size='12.84px' />
-      )}
-=======
     <div className={styles.star_tag}>
       <StarIcon size='14' />
       <span>{fixedRating}</span>
->>>>>>> abb7e3755b96af61595230f0b529a9ce51fa6a99
     </div>
   );
 }
