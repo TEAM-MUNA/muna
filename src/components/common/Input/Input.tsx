ff27410ce552dd6367f25aab0e3934fd20f2061c
accguy <accguy2190@naver.com>
2025-01-17 20:44:26 +0900

    fix: Input 컴포넌트 라벨 추가

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -28,11 +28,20 @@ export default function Input({
   icon = undefined,
   fullWidth = false,
 }: InputProps) {
+  // ID 생성
+  const inputId = `input-${label.replace(/\s+/g, "-").toLowerCase()}`;
   return (
     <div className={`${styles.container} ${fullWidth ? styles.full : ""}`}>
+      {/* 접근성을 위해 label 태그 추가 */}
+      {label && (
+        <label htmlFor={inputId} className='sr_only'>
+          {label}
+        </label>
+      )}
       {label && <p className={styles.label}>{label}</p>}
       <div className={styles.input_icon_container}>
         <input
+          id={inputId} // label과 연결 하기 위해 추가함
           type={type}
           name={name}
           className={styles.input}
